01ba6afa8e8e1cffce31af9fe278bce2dc8434ff
Aleckseev Grisha <grisha.zhivot@bk.ru>
2026-01-28 14:10:20 +0300

    Map collection in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -10,14 +10,33 @@ val newSolarSystem = arrayOf(
     "Pluto"
 )
 
+
 fun main(){
     val solarSystem = mutableListOf(
-        "Mercury", "Venus", "Earth", "Mars",
-        "Jupiter", "Saturn", "Uranus", "Neptune"
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2 ,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
     )
-    solarSystem.remove("Pluto")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+
+//    solarSystem["Jupiter"] = 78
+//    println(solarSystem["Jupiter"])
+
+//    println(solarSystem.size)
+//    solarSystem["Pluto"] = 5
+//    println(solarSystem.size)
+//    println(solarSystem["Pluto"])
+//    println(solarSystem.get("Theia"))
+//    null
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
 
 
 //    println("Pluto" in solarSystem)
