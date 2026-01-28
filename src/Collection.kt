a1851f4821840ec432ba5c6cbb3039478e4083db
Aleckseev Grisha <grisha.zhivot@bk.ru>
2026-01-28 12:42:41 +0300

    Arrays in Kotlin

---
+++ src/Collection.kt
@@ -0,0 +1,15 @@
+val newSolarSystem = arrayOf(
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+    "Pluto"
+)
+
+fun main(){
+    println(newSolarSystem[8])
+}
