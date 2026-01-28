d59500d87ea085bc9ed5eae21b6d3948cde14eeb
Aleckseev Grisha <grisha.zhivot@bk.ru>
2026-01-28 15:09:24 +0300

    fold() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -69,19 +69,23 @@ fun main(){
 //        println("${it.name} - $${it.price}")
 //    }
 
-    val groupedMenu = cookies.groupBy {it.softBaked}
-    val softBakedMenu = groupedMenu[true] ?: emptyList()
-    val cruchyMenu = groupedMenu[false] ?: emptyList()
+//    val groupedMenu = cookies.groupBy {it.softBaked}
+//    val softBakedMenu = groupedMenu[true] ?: emptyList()
+//    val cruchyMenu = groupedMenu[false] ?: emptyList()
+//
+//    println("Soft cookies:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+//    println("Crunchy cookies: ")
+//    cruchyMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
 
-    println("Soft cookies:")
-    softBakedMenu.forEach {
-        println("${it.name} - $${it.price}")
+    val totalPrice = cookies.fold(0.0){
+        total, cookie -> total + cookie.price
     }
-    println("Crunchy cookies: ")
-    cruchyMenu.forEach {
-        println("${it.name} - $${it.price}")
-    }
-
+    println("Total price: $${totalPrice}")
 
 }
 
